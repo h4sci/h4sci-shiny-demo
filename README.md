e0d1da70107d77eff34a863916b6e7e4bb50acd0
Matthias Bannert <bunny@hey.com>
2022-11-18 08:53:07 +0100

    minimal shiny demo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Basic Shiny Demo with Dashboard Layout
+
+
+- run the server
+- check out the docker compose demo
+
+
---
+++ ui.R
@@ -0,0 +1,9 @@
+library(shiny)
+library(shinydashboard)
+
+
+dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
